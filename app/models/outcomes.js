fd5214c03b349f5628a5a9dc439699abc1977f63
Jake Benilov <benilov@gmail.com>
2021-05-13 11:38:30 +0100

    Simplify Outcomes code

--- app/models/outcomes.js
+++ app/models/outcomes.js
@@ -1,16 +1,14 @@
 const path = require('path')
-const contactTypes = require(path.join(__dirname, '../data/reference/contact-types.json'))
 const outcomes = require(path.join(__dirname, '../data/reference/outcomes.json'))
 
 class Outcomes {
   static failureToComplyReasons(contactType) {
-    const outcomeCodes = contactType.outcomes
-    const outcomeDescriptions = outcomeCodes
+    return contactType
+      .outcomes
       .map(code => outcomes[code])
       .filter(outcome => outcome.attended === 'Y' || outcome.attended === null)
       .filter(outcome => outcome.compliantAcceptable === 'N')
       .map(outcome => outcome.description)
-    return outcomeDescriptions
   }
 }
 
