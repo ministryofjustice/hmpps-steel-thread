19026ea75789fd2ca59913e397c2404ce1d5ba6c
Jake Benilov <benilov@gmail.com>
2021-05-14 11:35:30 +0100

    Content designed descriptions for reasons for absence

--- app/models/outcomes.js
+++ app/models/outcomes.js
@@ -2,7 +2,7 @@ const path = require('path')
 const outcomes = require(path.join(__dirname, '../data/reference/outcomes.json'))
 
 class Outcomes {
-  static failureToComplyReasons(contactType) {
+  static failureToComplyReasons (contactType) {
     return contactType
       .outcomes
       .map(code => outcomes[code])
@@ -11,13 +11,32 @@ class Outcomes {
       .map(outcome => outcome.description)
   }
 
-  static acceptableAbsenceReasons(contactType) {
+  static acceptableAbsenceReasons (contactType) {
     return contactType
       .outcomes
       .map(code => outcomes[code])
       .filter(outcome => outcome.attended === 'N')
       .filter(outcome => outcome.compliantAcceptable === 'Y')
-      .map(outcome => outcome.description)
+      .map(outcome => this.mapFriendlyDescriptions(outcome.description))
+  }
+
+  static mapFriendlyDescriptions (description) {
+    const friendlyDescriptions = {
+      'Acceptable Absence - Court/Legal': 'Court or legal',
+      'Acceptable Absence - Employment': 'Employment',
+      'Acceptable Absence - Family/ Childcare': 'Childcare or family',
+      'Acceptable Absence - Holiday': 'Holiday or annual leave',
+      'Acceptable Absence - Medical': 'Medical',
+      'Acceptable Absence - Other': 'Other',
+      'Acceptable Absence - Religious': 'Religious',
+      'Acceptable Absence - RIC': 'Remanded in custody',
+      'Acceptable Absence - Stood Down': 'Stood down',
+      'Rescheduled - Offender Request': 'Rescheduled at the service user’s request',
+      'YOT Breach - Not Enforceable': 'Youth offending team breach - not enforceable',
+      'Acceptable Absence-Professional Judgement Decision': 'Professional judgement decision'
+    }
+
+    return friendlyDescriptions[description] || description
   }
 }
 
