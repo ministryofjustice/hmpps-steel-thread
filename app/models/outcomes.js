d96dc6f1525491ed6b38511a5924fa01d78309cf
Jake Benilov <benilov@gmail.com>
2021-05-19 11:06:48 +0100

    Update design of acceptable reasons page

--- app/models/outcomes.js
+++ app/models/outcomes.js
@@ -17,13 +17,18 @@ class Outcomes {
       .map(code => outcomes[code])
       .filter(outcome => outcome.attended === 'N')
       .filter(outcome => outcome.compliantAcceptable === 'Y')
+      .filter(outcome => outcome.description !== 'Acceptable Absence - Other') // Other gets its own option
+      .filter(outcome => outcome.description !== 'Acceptable Absence-Professional Judgement Decision') // all the remaining options are professional judgement decisions so this is too generic
+      .filter(outcome => !outcome.description.includes('Rescheduled')) // rescheduling is handled elsewhere in the app
+      .filter(outcome => !outcome.description.includes('YOT')) // not dealing with youth offenders at this stage
+      .filter(outcome => !outcome.description.includes('Suspended')) // not dealing with suspended sentences at this stage
       .map(outcome => this.mapFriendlyDescriptions(outcome.description))
   }
 
   static mapFriendlyDescriptions (description) {
     const friendlyDescriptions = {
       'Acceptable Absence - Court/Legal': 'Court or legal',
-      'Acceptable Absence - Employment': 'Employment',
+      'Acceptable Absence - Employment': 'Work or employment',
       'Acceptable Absence - Family/ Childcare': 'Childcare or family',
       'Acceptable Absence - Holiday': 'Holiday or annual leave',
       'Acceptable Absence - Medical': 'Medical',
