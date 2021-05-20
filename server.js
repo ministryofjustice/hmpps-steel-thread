739bc846045b29dde06b0c256e7b36ec90c45144
Jake Benilov <benilov@gmail.com>
2021-05-20 14:50:57 +0100

    Friendlier public URLs for timepicker JS and CSS

--- server.js
+++ server.js
@@ -120,8 +120,7 @@ app.use('/public', express.static(path.join(__dirname, '/public')))
 // Serve govuk-frontend in from node_modules (so not to break pre-extenstions prototype kits)
 app.use('/node_modules/govuk-frontend', express.static(path.join(__dirname, '/node_modules/govuk-frontend')))
 
-app.use('/node_modules/timepicker', express.static(path.join(__dirname, '/node_modules/timepicker')))
-app.use('/public/stylesheets/node_modules/timepicker', express.static(path.join(__dirname, '/node_modules/timepicker')))
+app.use(['/public/javascripts/timepicker', '/public/stylesheets/timepicker'], express.static(path.join(__dirname, '/node_modules/timepicker')))
 
 // Set up documentation app
 if (useDocumentation) {
