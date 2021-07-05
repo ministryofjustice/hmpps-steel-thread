5a1b8fe2fd32bbe00c3d148a0bc264ecac8bb807
Paul Hayes <fofr@users.noreply.github.com>
2021-07-05 12:26:38 +0100

    Generate highest risk of serious harm from community values

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -8,7 +8,25 @@ const contactHistoryDefaults = (map, su) => {
   return map
 }
 
+const highestRiskOfSeriousHarm = (su) => {
+  const riskValue = {
+    'Low': 0,
+    'Medium': 1,
+    'High': 2,
+    'Very high': 3
+  }
+
+  const compareRisk = (a, b) => {
+    if (riskValue[a.text] < riskValue[b.text]) { return -1 }
+    if (riskValue[a.text] > riskValue[b.text]) { return 1 }
+    return 0
+  }
+
+  su.riskOfSeriousHarmLevel = su.riskOfHarm.map(r => r.inCommunity).sort(compareRisk).pop()
+}
+
 const sessionCases = cases.concat(fakeCases(10))
+sessionCases.forEach(su => highestRiskOfSeriousHarm(su))
 
 module.exports = {
   features: {
